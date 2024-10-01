490a554ecd37b440ddbfdd24142c610a1253edf6
Lusine13 <lusinevardanyan.a@gmail.com>
2024-09-30 22:28:37 -0700

    creating project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Register from '../pages/register/index.js';
+import Header from './components/global/Header';
+import './styles/global.css';
+const App = () => {
+    return (
+        <div id='divContainer'>
+            <Header />
+            <Register />
+        </div>
+    )
+};
+
+export default App;
